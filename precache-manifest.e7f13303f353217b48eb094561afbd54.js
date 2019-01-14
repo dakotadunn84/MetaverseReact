45ef7aab4c0f11dbb139f33ca29fef50abfd3ea1
Dakota <dakotadunn84@gmail.com>
2019-01-14 15:43:43 -0800

    Updates

---
+++ precache-manifest.e7f13303f353217b48eb094561afbd54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MetaverseReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99888845fcd99122d625f81f870627cf",
+    "url": "/MetaverseReact/static/media/Space Background.99888845.jpg"
+  },
+  {
+    "revision": "a379e65d2b1cadac0a1d",
+    "url": "/MetaverseReact/static/js/runtime~main.a379e65d.js"
+  },
+  {
+    "revision": "9f398834e2472af77f0b",
+    "url": "/MetaverseReact/static/js/main.9f398834.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/MetaverseReact/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "9f398834e2472af77f0b",
+    "url": "/MetaverseReact/static/css/main.eed9b4fb.chunk.css"
+  },
+  {
+    "revision": "5d05dcda03afec4aa8eaaeaec743139f",
+    "url": "/MetaverseReact/index.html"
+  }
+];
